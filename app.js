31837f58d4e6fa6414dcada9838d5f4e14a3d3a5
Ava Feider <af10712014@gmail.com>
2024-11-12 15:43:51 -0600

    edit names

--- app.js
+++ app.js
@@ -6,37 +6,37 @@ let team1 = {
     worldwidefansinmillions: 798};
 
 let team2 = {
-    teamname: “Barcelona”,
+    teamname: "Barcelona",
     city: “Barcelona”,
     country: “Spain”,
-    top scorers: [“Messi”, “Suarez”, “Puyol”],
-    worldwidefans-inmillions: 738};
+    topscorers: [“Messi”, “Suarez”, “Puyol”],
+    worldwidefansinmillions: 738};
     
     let team3 = {
     teamname: “Manchester United”,
     city: “Manchester”,
     country: “England”,
-    top scorers: [“Contona”, “Rooney”, “Ronaldo”],
-    worldwidefans-inmillions: 755};
+    topscorers: [“Contona”, “Rooney”, “Ronaldo”],
+    worldwidefansinmillions: 755};
     
     let team4 = {
     teamname: “Manchester City”,
     city: “Manchester”,
     country: “England”,
-    top scorers: [“Sterling”, “Aguero”, “Haaland”],
-    worldwidefans-inmillions: 537};
+    topscorers: [“Sterling”, “Aguero”, “Haaland”],
+    worldwidefansinmillions: 537};
     
     let team4 = {
     teamname: “Brazil National Team”,
     city: “Not applicable”,
     country: “Brazil”,
-    top scorers: [“Ronaldinho”, “Cafu”, “Bebeto”],
-    worldwidefans-inmillions: 950};
+    topscorers: [“Ronaldinho”, “Cafu”, “Bebeto”],
+    worldwidefansinmillions: 950};
     
     let team4 = {
     teamname: “Brazil National Team”,
     city: “Not applicable”,
     country: “Brazil”,
-    top scorers: [“Ronaldinho”, “Cafu”, “Bebeto”],
-    worldwidefans-inmillions: 950};
+    topscorers: [“Ronaldinho”, “Cafu”, “Bebeto”],
+    worldwidefansinmillions: 950};
     
