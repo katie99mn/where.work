c7fc092dd3c5389d9a7ff4affd462e585e5de6f7
Ava Feider <af10712014@gmail.com>
2024-11-12 15:43:05 -0600

    add teams

---
+++ app.js
@@ -0,0 +1,42 @@
+let team1 = {
+    teamname: "Real Madrid",
+    city: "Madrid",
+    country: "Spain",
+    topscorers: ["Ronaldo", "Benzema", "Hazard"],
+    worldwidefansinmillions: 798};
+
+let team2 = {
+    teamname: “Barcelona”,
+    city: “Barcelona”,
+    country: “Spain”,
+    top scorers: [“Messi”, “Suarez”, “Puyol”],
+    worldwidefans-inmillions: 738};
+    
+    let team3 = {
+    teamname: “Manchester United”,
+    city: “Manchester”,
+    country: “England”,
+    top scorers: [“Contona”, “Rooney”, “Ronaldo”],
+    worldwidefans-inmillions: 755};
+    
+    let team4 = {
+    teamname: “Manchester City”,
+    city: “Manchester”,
+    country: “England”,
+    top scorers: [“Sterling”, “Aguero”, “Haaland”],
+    worldwidefans-inmillions: 537};
+    
+    let team4 = {
+    teamname: “Brazil National Team”,
+    city: “Not applicable”,
+    country: “Brazil”,
+    top scorers: [“Ronaldinho”, “Cafu”, “Bebeto”],
+    worldwidefans-inmillions: 950};
+    
+    let team4 = {
+    teamname: “Brazil National Team”,
+    city: “Not applicable”,
+    country: “Brazil”,
+    top scorers: [“Ronaldinho”, “Cafu”, “Bebeto”],
+    worldwidefans-inmillions: 950};
+    
